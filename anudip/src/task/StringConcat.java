3c4fa3ee6190fd0981eb7c1654ce3dd6a50b5f6d
Kiran Jyothir <Kiran Jyothir@DESKTOP-O61P8BM.bbrouter>
2021-10-31 04:18:10 +0530

    java prog string concat

---
+++ anudip/src/task/StringConcat.java
@@ -0,0 +1,15 @@
+package task;
+
+public class StringConcat 
+{
+
+	public static void main(String[] args) 
+	{
+		String fname;
+		String fullname;
+		fname ="Kiran";
+		fullname = fname.concat(" Jyothir");
+		System.out.println(fullname);
+	}
+
+}
