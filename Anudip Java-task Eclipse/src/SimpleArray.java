5799a6fa30bc5d49ac6f41afae15f249df242d78
Kiran Jyothir <Kiran Jyothir@DESKTOP-O61P8BM.bbrouter>
2021-10-31 05:30:53 +0530

    java task

--- Anudip Java-task Eclipse/src/SimpleArray.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class SimpleArray 
-{
-	static int a[]={1,2,3,4,5};
-    public static void main(String[] args) {
-       System.out.println(Arrays.toString(a)); 
-    }
-}
